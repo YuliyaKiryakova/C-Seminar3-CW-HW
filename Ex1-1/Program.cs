fb9ceff819906050148a8686db522caf81066d76
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-17 18:17:33 +0300

    Ex1-1

---
+++ Ex1-1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* как число сделать массивом, то есть, чтобы каждая цифра в числе имела свой индекс
+*/
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+        else
+            Console.Write(".");
+    }
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int temp = num;
+
+int count = 0;
+while (temp != 0)
+{
+    temp = temp / 10; // temp/=10;
+    count++;
+}
+
+int[] array = new int[count];
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    array[i] = num % 10;
+    num /= 10;
+}
+
+PrintArray(array);
+Console.Write($"\n{array[2]}");
